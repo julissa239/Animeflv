0098fa51304e457b645a1510d8198b759cf05c49
julissa239 <62450975+julissa239@users.noreply.github.com>
2020-03-23 16:10:40 -0600

    Update and rename app/helpers.ts to  app/helpers.ts

--- app/helpers.ts
+++ app/helpers.ts
@@ -1,7 +1,6 @@
 import mongoose = require("mongoose");
 import { resolve } from "dns";
-
-const uri: string = "mongodb://localhost/animeUnah";
+const uri: string = "mongodb+srv://Julissa:dany.1996@cluster0-shzfi.azure.mongodb.net/animeUnah?retryWrites=true&w=majority";
 
 export const connectMongoDB  = new Promise<void>(resolve => {
     mongoose.connect(uri,{ useNewUrlParser:true, useUnifiedTopology: true }, (err: any) => {
