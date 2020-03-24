2858491b58362f3958a064b4aa4a48beb37e812b
julissa239 <62450975+julissa239@users.noreply.github.com>
2020-03-23 21:09:43 -0600

    Update and rename app/app.ts to  app/app.ts

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,7 @@
 import { CreateEpisodio } from "./Episodios";
 import {CreateAnime} from "./Anime"
 
-CreateEpisodio("Nombre Episodio",1,"24:00","Comenario Episodio",6)
+CreateEpisodio("Nombre Episodio",1,"24:00","Comentario Episodio",6)
 
 CreateAnime("The Rising of Shield Hero","Nombre Episodio","Accion","Gamers");
 
